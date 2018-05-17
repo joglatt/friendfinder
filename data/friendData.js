12c0febceb6b38d30eb078e5a319542d9c8a7c06
joglatt <34425753+joglatt@users.noreply.github.com>
2018-05-16 20:58:16 -0400

    inception

--- data/friendData.js
+++ data/friendData.js
@@ -1,26 +1,39 @@
 var friendData = [
   {
-    name: "bill",
-    array: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    name: "karl",
+    array: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    difference: 0
   },
   {
-    name: "Joe",
-    array: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    name: "joe",
+    array: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    difference: 0
   },
   {
-    name: "sam",
-    array: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    name: "grey",
+    array: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    difference: 0
   }
 ];
-
+var total;
 function checker() {
-  for (array in friendData[0]) {
-    for (var i = 1; i < friendData.length; i++) {
-      for (array[i] in friendData) {
-        
+// loops through objects starting with the second one in the array
+  for (var j = 1; j < friendData.length; j++) {
+    total = 0;
+//loops through the array in the first objects in friendData
+   for (var i = 0; i < friendData[0].array.length; i++) {
+//loops through the array in the object being looped through by the first loop
+      for (var k = 0; k < friendData[j].array.length; k++) {
+        total += Math.abs(friendData[0].array[i] - friendData[j].array[k]);
+        console.log(total);
+        friendData[j].difference= total;
+        console.log("diff" + friendData[j].difference);
+
       }
     }
   }
 }
+
+checker();
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendData;
