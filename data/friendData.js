e4fa3bcfe193334dfb2cf4064c9f0f7685c62a2b
joglatt <34425753+joglatt@users.noreply.github.com>
2018-05-20 19:24:23 -0400

    image correct

--- data/friendData.js
+++ data/friendData.js
@@ -1,32 +1,36 @@
 var friendData = [
   {
-    name: "milton",
-    image:'https://i0.wp.com/andrebartholomeufernandes.com/wp-content/uploads/2016/04/Milton-Friedman.jpg?fit=1024%2C768&ssl=1',
+    name: "Milton",
+    image:
+      "https://i0.wp.com/andrebartholomeufernandes.com/wp-content/uploads/2016/04/Milton-Friedman.jpg?fit=1024%2C768&ssl=1",
     array: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     difference: 0
   },
 
   {
-    name: "ron",
-    image:'https://upload.wikimedia.org/wikipedia/commons/1/16/Official_Portrait_of_President_Reagan_1981.jpg',
+    name: "Ron",
+    image:
+      "https://i0.wp.com/www.nationalreview.com/wp-content/uploads/2015/06/ronald-reagan-example-2.jpg?fit=788%2C460&ssl=1",
     array: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     difference: 0
   },
   {
     name: "John",
-    image:'https://boerse.ard.de/aktien/john-maynard-keynes100~_pd-1405926299174_v-z-a-par-a-l.jpg',
+    image:
+      "https://boerse.ard.de/aktien/john-maynard-keynes100~_pd-1405926299174_v-z-a-par-a-l.jpg",
     array: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
     difference: 0
   },
   {
-    name: "vladimir",
-    image:'https://images.jacobinmag.com/2017/05/24182604/lenin.jpg',
+    name: "Vladimir",
+    image: "https://images.jacobinmag.com/2017/05/24182604/lenin.jpg",
     array: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
     difference: 0
   },
   {
-    name: "karl",
-    image:'http://d24fkeqntp1r7r.cloudfront.net/wp-content/uploads/2018/05/05060720/KarlMarx.jpg',
+    name: "Karl",
+    image:
+      "http://d24fkeqntp1r7r.cloudfront.net/wp-content/uploads/2018/05/05060720/KarlMarx.jpg",
     array: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
     difference: 0
   }
